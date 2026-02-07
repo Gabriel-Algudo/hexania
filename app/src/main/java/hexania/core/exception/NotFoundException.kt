96a994852ad8813adec3e714d0a24f0ab4bd897d
Gabriel <gabriel.algudo6@gmail.com>
2026-02-07 19:01:01 +0100

    reset de l'app

--- app/src/main/java/hexania/core/exception/NotFoundException.kt
+++
@@ -1,4 +0,0 @@
-package hexania.core.exception
-
-class NotFoundException(message : String) : Exception(message) {
-}
--- app/src/main/java/hexania/core/exception/OnClickException.kt
+++
@@ -1,4 +0,0 @@
-package hexania.core.exception
-
-class OnClickException(message : String) : Exception(message){
-}
